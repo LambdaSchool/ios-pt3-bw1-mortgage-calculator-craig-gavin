ac30fbaede9d7f483eaa28a057ee635a9e48c23b
Craig Swanson <cswanson02@me.com>
2019-10-16 22:53:57 -0500

    Created Model file.

---
+++ PalmMortgageCalc/PalmMortgageCalc/Loan.swift
@@ -0,0 +1,28 @@
+//
+//  Loan.swift
+//  PalmMortgageCalc
+//
+//  Created by Craig Swanson on 10/16/19.
+//  Copyright © 2019 Gavin Murphy. All rights reserved.
+//
+
+import Foundation
+
+// I named it "Loan" instead of Mortgage because I think it is more general to any loan, not just a mortgage
+
+struct Loan: Codable {
+    var principal: Double   // the initial loan value and the principal remaining after each payment
+    var years: Double       // number of years in the term of the loan
+    var rate: Double        // interest rate of the loan
+    var downPayment: Double?  //  additional down payment the user makes upfront
+    var paymentsPerPeriod: Double   //  number of payments per year. Usually 12 but doesn't have to be
+    var additionalPrincipal: Double?  // for each payment, user could add additional money to reduce principal
+    
+    // for the initializer, I set the default paymentsPerPeriod to 12. I'm not great with initializers though
+    init(principal: Double, years: Double, rate: Double, downPayment: Double?, paymentsPerPeriod: Double = 12, additionalPrincipal: Double?) {
+        self.principal = principal
+        self.years = years
+        self.rate = rate
+        self.paymentsPerPeriod = paymentsPerPeriod
+    }
+}
