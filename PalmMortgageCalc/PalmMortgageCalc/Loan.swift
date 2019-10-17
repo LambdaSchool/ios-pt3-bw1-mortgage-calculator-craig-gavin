08da9113f2bbf392b9647004583e6a11b283b659
Craig Swanson <cswanson02@me.com>
2019-10-17 16:36:07 -0500

    I am currently writing the formulas and I updated the model to change the two variables that were optional.  They are now not optional with default values of 0.

--- PalmMortgageCalc/PalmMortgageCalc/Loan.swift
+++ PalmMortgageCalc/PalmMortgageCalc/Loan.swift
@@ -14,15 +14,17 @@ struct Loan: Codable {
     var principal: Double   // the initial loan value and the principal remaining after each payment
     var years: Double       // number of years in the term of the loan
     var rate: Double        // interest rate of the loan
-    var downPayment: Double?  //  additional down payment the user makes upfront
+    var downPayment: Double  //  additional down payment the user makes upfront
     var paymentsPerPeriod: Double   //  number of payments per year. Usually 12 but doesn't have to be
-    var additionalPrincipal: Double?  // for each payment, user could add additional money to reduce principal
+    var additionalPrincipal: Double  // for each payment, user could add additional money to reduce principal
     
-    // for the initializer, I set the default paymentsPerPeriod to 12. I'm not great with initializers though
-    init(principal: Double, years: Double, rate: Double, downPayment: Double?, paymentsPerPeriod: Double = 12, additionalPrincipal: Double?) {
+    // for the initializer, I set the default paymentsPerPeriod to 12.  I updated it to also set defaults for the downPayment and additionalPrincipal.  They used to be optionals but now they are not. I'm not great with initializers though
+    init(principal: Double, years: Double, rate: Double, downPayment: Double = 0, paymentsPerPeriod: Double = 12, additionalPrincipal: Double = 0) {
         self.principal = principal
         self.years = years
         self.rate = rate
+        self.downPayment = downPayment
         self.paymentsPerPeriod = paymentsPerPeriod
+        self.additionalPrincipal = additionalPrincipal
     }
 }
