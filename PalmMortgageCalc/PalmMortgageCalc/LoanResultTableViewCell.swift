10f5e93c236d5378c0493008d88989f636ecebf6
Craig Swanson <cswanson02@me.com>
2019-10-18 15:28:13 -0500

    Created the view controller files and wired everything up.

---
+++ PalmMortgageCalc/PalmMortgageCalc/LoanResultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoanResultTableViewCell.swift
+//  PalmMortgageCalc
+//
+//  Created by Craig Swanson on 10/18/19.
+//  Copyright © 2019 Gavin Murphy. All rights reserved.
+//
+
+import UIKit
+
+class LoanResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PalmMortgageCalc/PalmMortgageCalc/View Controllers/LoanCalculatorViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LoanCalculatorViewController.swift
+//  PalmMortgageCalc
+//
+//  Created by Craig Swanson on 10/18/19.
+//  Copyright © 2019 Gavin Murphy. All rights reserved.
+//
+
+import UIKit
+
+class LoanCalculatorViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    
+    // MARK: Outlets
+    @IBOutlet weak var loanTypeTextField: UITextField!
+    @IBOutlet weak var loanPrincipalTextField: UITextField!
+    @IBOutlet weak var loanTermTextField: UITextField!
+    @IBOutlet weak var loanInterestRateTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
