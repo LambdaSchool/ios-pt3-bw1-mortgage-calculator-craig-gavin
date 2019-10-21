dd6271e1ff5761b83e2a66170e0a0638795c610f
Gavin Murphy <gavincmurphy@gmail.com>
2019-10-20 21:36:02 -0700

    Loan Detail View Controller changes
    Changed the text fields to labels on the storyboard. Set up outlets in the view contoller. Started a updateViews func that I need to finish.  Named the segue from the LoanLibrary view controller to the LoanDetail view controller "ShowLoanDetailSegue".

--- PalmMortgageCalc/PalmMortgageCalc/View Controllers/LoanDetailViewController.swift
+++ PalmMortgageCalc/PalmMortgageCalc/View Controllers/LoanDetailViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class LoanDetailViewController: UIViewController {
     
+    // MARK: Outlets
     
+    @IBOutlet weak var loanPrincipalLabel: UILabel!
+    @IBOutlet weak var loanTermLabel: UILabel!
+    @IBOutlet weak var loanInterestRateLabel: UILabel!
+    @IBOutlet weak var downPaymentLabel: UILabel!
+    @IBOutlet weak var additionalPaymentLabel: UILabel!
     
+    var loan: Loan? {
+        didSet {
+            updateViews()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateViews()
         // Do any additional setup after loading the view.
+    }
+    
+    private func updateViews() {
+        guard let loan = loan, isViewLoaded else { return } // not sure if this is right guard let
+        
+        // loanPrincipalLabel.text = loan.loan **this is wrong need to figure this out, and other IBOutlets as well
     }
     
 }
