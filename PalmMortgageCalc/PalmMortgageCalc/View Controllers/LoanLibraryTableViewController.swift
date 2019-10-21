0d8998662e5123c873c9828989fa0efe0616a144
Craig Swanson <cswanson02@me.com>
2019-10-21 10:53:05 -0500

    Fixed delegate to pass information from Loan Calculator to Loan Library

--- PalmMortgageCalc/PalmMortgageCalc/View Controllers/LoanLibraryTableViewController.swift
+++ PalmMortgageCalc/PalmMortgageCalc/View Controllers/LoanLibraryTableViewController.swift
@@ -58,15 +58,15 @@ class LoanLibraryTableViewController: UITableViewController {
     }
     */
 
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ModifyLoanFromLibrarySegue" {
+            guard let loanCalculatorVC = segue.destination as? LoanCalculatorViewController else { fatalError() }
+            loanCalculatorVC.delegate = self
+        }
     }
-    */
 
 }
 
